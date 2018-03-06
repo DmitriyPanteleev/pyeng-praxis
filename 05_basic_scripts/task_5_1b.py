45469c0a5cb2d87f9b62611e7f4e1361b999f918
piligrim <pant-dm@yandex.ru>
2018-03-06 17:08:43 +0300

    by argument Вт мар  6 17:08:31 MSK 2018

--- 05_basic_scripts/task_5_1b.py
+++ 05_basic_scripts/task_5_1b.py
@@ -9,3 +9,32 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+
+from sys import argv
+
+# Get address string and CIDR string from user input
+# userNetwork = input('Input network (like 10.1.1.0/24): ')
+(addrString, cidrString) = argv[1].split('/')
+
+# Split address into octets and turn CIDR into int
+addr = addrString.split('.')
+cidr = int(cidrString)
+
+# Initialize the netmask and calculate based on CIDR mask
+mask = [0, 0, 0, 0]
+for i in range(cidr):
+	mask[int(i/8)] = mask[int(i/8)] + (1 << (7 - i % 8))
+
+# Initialize net and binary and netmask with addr to get network
+net = []
+for i in range(4):
+	net.append(int(addr[i]) & mask[i])
+
+# Print information
+print('Network: ')
+print('{:10} {:10} {:10} {:10}'.format(net[0], net[1], net[2], net[3]))
+print('{:010b} {:010b} {:010b} {:010b}'.format(int(net[0]), int(net[1]), int(net[2]), int(net[3])))
+
+print('Mask: ')
+print('{:10} {:10} {:10} {:10}'.format(mask[0], mask[1], mask[2], mask[3]))
+print('{:010b} {:010b} {:010b} {:010b}'.format(int(mask[0]), int(mask[1]), int(mask[2]), int(mask[3])))
