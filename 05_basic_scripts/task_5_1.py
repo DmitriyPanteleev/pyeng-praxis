48eb23381e5296db9d4dbed0efe0b7c7acac2e67
piligrim <pant-dm@yandex.ru>
2018-03-06 15:31:51 +0300

    Вт мар  6 15:31:30 MSK 2018

--- 05_basic_scripts/task_5_1.py
+++ 05_basic_scripts/task_5_1.py
@@ -19,3 +19,33 @@ Mask:
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
+
+# Get address string and CIDR string from user input
+userNetwork = input('Input network (like 10.1.1.0/24): ')
+(addrString, cidrString) = userNetwork.split('/')
+
+# Split address into octets and turn CIDR into int
+addr = addrString.split('.')
+cidr = int(cidrString)
+
+# Initialize the netmask and calculate based on CIDR mask
+mask = [0, 0, 0, 0]
+for i in range(cidr):
+	mask[int(i/8)] = mask[int(i/8)] + (1 << (7 - i % 8))
+
+# Initialize net and binary and netmask with addr to get network
+net = []
+for i in range(4):
+	net.append(int(addr[i]) & mask[i])
+
+# Duplicate net into broad array, gather host bits, and generate broadcast
+broad = list(net)
+brange = 32 - cidr
+for i in range(brange):
+	broad[int(3 - i/8)] = broad[int(3 - i/8)] + (1 << (i % 8))
+
+# Print information, mapping integer lists to strings for easy printing
+print ("Address:   " , addrString)
+print ("Netmask:   " , ".".join(map(str, mask)))
+print ("Network:   " , ".".join(map(str, net)))
+print ("Broadcast: " , ".".join(map(str, broad)))
