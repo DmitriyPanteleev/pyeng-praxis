aff3d9fdd414bc7cb75544908b48ad23439da0ea
piligrim <pant-dm@yandex.ru>
2018-03-19 15:48:20 +0300

    2018-03-19

--- 06_control_structures/task_6_1a.py
+++ 06_control_structures/task_6_1a.py
@@ -16,3 +16,25 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 '''
+
+ipAddress = input('Enter IP Address: ')
+octets = ipAddress.split('.')
+
+# Test ip for correct input
+
+if len(octets) != 4 :
+    print('Incorrect IPv4 address')
+for octet in octets :
+    if int(octet) < 0 or int(octet) > 255 :
+        print('Incorrect IPv4 address')
+        break
+if ipAddress == '0.0.0.0' :
+    print('unassigned')
+elif ipAddress == '255.255.255.255' :
+    print('local broadcast')
+elif int(octets[0]) < 224 :
+    print('unicast')
+elif int(octets[0]) < 239 :
+    print('multicast')
+else :
+    print('unused')
