f92202cd9ded467767060085ab32f3155eb26e14
piligrim <pant-dm@yandex.ru>
2018-03-22 15:55:55 +0300

    Чт мар 22 15:55:40 MSK 2018

--- 06_control_structures/task_6_3.py
+++ 06_control_structures/task_6_3.py
@@ -39,18 +39,31 @@ fast_int = {
         '0/14': '11',
         '0/16': '17',
         '0/17': '150'
-    },
+              },
     'trunk': {
         '0/1': ['add', '10', '20'],
         '0/2': ['only', '11', '30'],
         '0/4': ['del', '17']
-    }
-}
+             }
+            }
 
 for intf, vlan in fast_int['access'].items():
     print('interface FastEthernet' + intf)
     for command in access_template:
         if command.endswith('access vlan'):
             print(' {} {}'.format(command, vlan))
+        else:
+            print(' {}'.format(command))
+
+for intf, vlan in fast_int['trunk'].items():
+    print('interface FastEthernet' + intf)
+    for command in trunk_template:
+        if command.endswith('allowed vlan'):
+            if vlan[0] == 'del':
+                print(' {} remove {}'.format(command, ' '.join(vlan[1:])))
+            if vlan[0] == 'add':
+                print(' {} add {}'.format(command, ' '.join(vlan[1:])))
+            if vlan[0] == 'only':
+                print(' {} {}'.format(command, ' '.join(vlan[1:])))
         else:
             print(' {}'.format(command))
