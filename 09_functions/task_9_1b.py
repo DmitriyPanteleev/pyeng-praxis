044e73f12fc8fee9d3b14062a7f2ba8752f74675
piligrim <pant-dm@yandex.ru>
2018-05-23 17:12:39 +0300

    Ср май 23 17:12:19 MSK 2018

--- 09_functions/task_9_1b.py
+++ 09_functions/task_9_1b.py
@@ -21,8 +21,9 @@
 
 '''
 
+# Функции и начальные данные
 
-def generate_access_config(access):
+def generate_access_config(access, psecurity = False):
     '''
     access - словарь access-портов,
     для которых необходимо сгенерировать конфигурацию, вида:
@@ -34,9 +35,7 @@ def generate_access_config(access):
         - если значение True, то настройка выполняется с добавлением шаблона port_security
         - если значение False, то настройка не выполняется
 
-    Функция возвращает словарь:
-    - ключи: имена интерфейсов, вида 'FastEthernet0/1'
-    - значения: список команд, который надо выполнить на этом интерфейсе
+    Возвращает список всех команд, которые были сгенерированы на основе шаблона
     '''
 
     access_template = [
@@ -50,7 +49,22 @@ def generate_access_config(access):
         'switchport port-security violation restrict',
         'switchport port-security'
     ]
+    
+    result_dic = {}
 
+    for key in access.keys():
+        result_list = []
+        result_list.append('switchport mode access')
+        result_list.append('switchport access vlan {}'.format(access[key]))
+        result_list.append('switchport nonegotiate')
+        result_list.append('spanning-tree portfast')
+        result_list.append('spanning-tree bpduguard enable')
+        if psecurity :
+            for item in port_security:
+                result_list.append(item)
+        result_dic.update({key:result_list})
+
+    return result_dic
 
 access_dict = {
     'FastEthernet0/12': 10,
@@ -58,3 +72,12 @@ access_dict = {
     'FastEthernet0/16': 17,
     'FastEthernet0/17': 150
 }
+
+# Основное тело программы
+
+result = generate_access_config(access_dict, True)
+
+#for item in result:
+#    print(item)
+
+print(result)
