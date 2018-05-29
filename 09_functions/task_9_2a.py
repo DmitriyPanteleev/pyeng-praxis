f000f2a061752515fea7d98ee7cc27d17b9e3d5a
piligrim <pant-dm@yandex.ru>
2018-05-29 16:22:20 +0300

    Вт май 29 16:22:05 MSK 2018

--- 09_functions/task_9_2a.py
+++ 09_functions/task_9_2a.py
@@ -14,27 +14,38 @@
 
 '''
 
-
 def generate_trunk_config(trunk):
     '''
-    trunk - словарь trunk-портов,
-    для которых необходимо сгенерировать конфигурацию, вида:
-        { 'FastEthernet0/1':[10,20],
-          'FastEthernet0/2':[11,30],
-          'FastEthernet0/4':[17] }
+    trunk - словарь trunk-портов, для которых необходимо сгенерировать конфигурацию
 
-    Возвращает словарь:
-    - ключи: имена интерфейсов, вида 'FastEthernet0/1'
-    - значения: список команд, который надо выполнить на этом интерфейсе
+    Возвращает список всех команд, которые были сгенерированы на основе шаблона
     '''
     trunk_template = [
         'switchport trunk encapsulation dot1q', 'switchport mode trunk',
         'switchport trunk native vlan 999', 'switchport trunk allowed vlan'
     ]
+    result_dict = {}
 
+    for key in trunk.keys():
+        result_list = []
+        # result_list.append('interface {}'.format(key))
+        result_list.append('switchport trunk encapsulation dot1q')
+        result_list.append('switchport mode trunk')
+        result_list.append('switchport trunk native vlan 999')
+        istr = ''
+        for i in trunk[key]: istr = istr + str(i) + ' '
+        result_list.append('switchport trunk allowed vlan {}'.format(istr))
+        result_dict.update({key:result_list})
+    
+    return result_dict
 
 trunk_dict = {
     'FastEthernet0/1': [10, 20, 30],
     'FastEthernet0/2': [11, 30],
     'FastEthernet0/4': [17]
 }
+# Основное тело программы
+
+result = generate_trunk_config(trunk_dict)
+
+print(result)
