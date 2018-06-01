2213ba646134b5f0ccbfbc9552e7f4781b77f64a
piligrim <pant-dm@yandex.ru>
2018-06-01 14:23:55 +0300

    Пт июн  1 14:23:43 MSK 2018

--- 09_functions/task_9_3c.py
+++ 09_functions/task_9_3c.py
@@ -22,49 +22,38 @@
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
 
+from sys import argv
+
+# filename = str(argv[1:]).strip('[]')[1:-1]
+
 RealConfigFile = 'config_sw1.txt'
 print(RealConfigFile)
 
 def get_int_vlan_map(filename):
-    ResultDictTrunk = {}
-    ResultDictAccess = {}
+    ResultDict = {}
     with open(filename, 'r') as f:
-        Trigger = 'Empty'
-        VLANsList = []
+        Trigger = False
+        InterfaceConfigList = []
         for line in f:
             if line.find('interface') != -1 :
-                Trigger = 'Interface'
+                Trigger = True
                 InterfaceName = line.strip('\n')
-                VLANsList = []
+                InterfaceConfigList = []
                 continue
             if line.find('!') != -1 :
-                if Trigger == 'Access':
-                    ResultDictAccess.update({InterfaceName:VLANsList})
-                    VLANsList = []
-                if Trigger == 'Trunk':
-                    ResultDictTrunk.update({InterfaceName:VLANsList})
-                    VLANsList = []
-                Trigger = 'Empty'
+                if Trigger:
+                    ResultDict.update({InterfaceName:InterfaceConfigList})
+                    InterfaceConfigList = []
+                Trigger = False
                 continue
-            if line.find('access vlan') != -1 :
-                VLANsList = [int(s) for s in line.split() if s.isdigit()]
-                Trigger = 'Access'
-            if line.find('trunk allowed vlan') != -1 :
-                line = line.replace(',',' ')
-                VLANsList = [int(s) for s in line.split() if s.isdigit()]
-                Trigger = 'Trunk'
-    return ResultDictAccess, ResultDictTrunk
+            InterfaceConfigList.append(line.strip('\n'))
+    return ResultDict
 
 # Основная программа
 
-RealResultAccess, RealResultTrunk = get_int_vlan_map(RealConfigFile)
+RealResult = get_int_vlan_map(RealConfigFile)
 
-print('Access:')
-for InterfaceName in RealResultAccess.keys():
-    print(InterfaceName+': ', end = '')
-    print(RealResultAccess[InterfaceName])
-
-print('Trunk:')
-for InterfaceName in RealResultTrunk.keys():
-    print(InterfaceName+': ', end = '')
-    print(RealResultTrunk[InterfaceName])
+for InterfaceName in RealResult.keys():
+    print(InterfaceName)
+    for Command in RealResult[InterfaceName]:
+        print(Command)
