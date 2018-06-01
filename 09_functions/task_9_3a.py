a7da84328ca386cb416a597a3812b7c4ebdedf5a
piligrim <pant-dm@yandex.ru>
2018-06-01 15:05:59 +0300

    Пт июн  1 15:05:46 MSK 2018

--- 09_functions/task_9_3a.py
+++ 09_functions/task_9_3a.py
@@ -23,3 +23,54 @@
 
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 '''
+RealConfigFile = 'config_sw2.txt'
+print(RealConfigFile)
+
+def get_int_vlan_map(filename):
+    ResultDictTrunk = {}
+    ResultDictAccess = {}
+    with open(filename, 'r') as f:
+        Trigger = 'Empty'
+        VLANsList = []
+        for line in f:
+            if line.find('interface') != -1 :
+                Trigger = 'Interface'
+                InterfaceName = line.strip('\n')
+                VLANsList = []
+                continue
+            if line.find('!') != -1 :
+                if Trigger == 'Access':
+                    if VLANsList == []:
+                        ResultDictAccess.update({InterfaceName:[1]})
+                    else:
+                        ResultDictAccess.update({InterfaceName:VLANsList})
+                    VLANsList = []
+                if Trigger == 'Trunk':
+                    ResultDictTrunk.update({InterfaceName:VLANsList})
+                    VLANsList = []
+                Trigger = 'Empty'
+                continue
+            if line.find('mode access') != -1 :
+                Trigger = 'Access'
+            if line.find('access vlan') != -1 :
+                VLANsList = [int(s) for s in line.split() if s.isdigit()]
+                Trigger = 'Access'
+            if line.find('trunk allowed vlan') != -1 :
+                line = line.replace(',',' ')
+                VLANsList = [int(s) for s in line.split() if s.isdigit()]
+                Trigger = 'Trunk'
+    return ResultDictAccess, ResultDictTrunk
+
+# Основная программа
+
+RealResultAccess, RealResultTrunk = get_int_vlan_map(RealConfigFile)
+
+print('Access:')
+for InterfaceName in RealResultAccess.keys():
+    print(InterfaceName+': ', end = '')
+    print(RealResultAccess[InterfaceName])
+
+print('Trunk:')
+for InterfaceName in RealResultTrunk.keys():
+    print(InterfaceName+': ', end = '')
+    print(RealResultTrunk[InterfaceName])
