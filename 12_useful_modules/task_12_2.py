ac4f05447964639083566fdc07918f4468844fbd
DmitriyPanteleev <pant-dm@yandex.ru>
2019-04-02 17:22:15 +0300

    first 12_2

--- 12_useful_modules/task_12_2.py
+++ 12_useful_modules/task_12_2.py
@@ -26,6 +26,36 @@ IP-адреса могут быть в формате:
 * список доступных IP-адресов
 * список недоступных IP-адресов
 
-
 Для выполнения задачи можно воспользоваться функцией check_ip_addresses из задания 12.1
 '''
+import subprocess
+from datetime import date
+from netaddr import iter_iprange
+
+def ping_ip(ip_address = '127.0.0.1', count = 3):
+    reply = subprocess.run('ping -c {count} -n {ip}'.format(count=count, ip=ip_address),
+                           shell = True,
+                           stdout = subprocess.PIPE,
+                           stderr = subprocess.PIPE)
+    if reply.returncode == 0:
+        return True, reply.stdout
+    else:
+        return False, reply.stdout + reply.stderr
+
+with open('check_ip_list', 'r') as chkList, \
+     open('{}_avlb_ip'.format(str(date.today())), 'w') as avIP, \
+     open('{}_unlb_ip'.format(str(date.today())), 'w') as unIP:
+     
+    for ipStr in chkList:
+        if ipStr.find('-') != -1 :
+            ipbeg = ipStr[:(ipStr.find('-'))-1]
+            ipend = ipStr[(ipStr.find('-'))+1:]
+            iprange = iter_iprange(ipbeg, ipend, step=1)
+            for ip in iprange:
+                answer = ping_ip(ip)
+                if answer[0]: avIP.write(ip)
+                else: unIP.write(ip)
+        answer = ping_ip(ip)
+        if answer[0]: avIP.write(ip)
+        else: unIP.write(ip)
+            
