be7d3851ad94314f911cd0165d1f9524000e697e
DmitriyPanteleev <pant-dm@yandex.ru>
2019-04-02 11:12:30 +0300

    final version 12_1

--- 12_useful_modules/task_12_1.py
+++ 12_useful_modules/task_12_1.py
@@ -16,29 +16,23 @@
 '''
 
 import subprocess
+from datetime import date
 
 def ping_ip(ip_address = '127.0.0.1', count = 3):
     reply = subprocess.run('ping -c {count} -n {ip}'.format(count=count, ip=ip_address),
                            shell = True,
                            stdout = subprocess.PIPE,
-                           stderr = subprocess.PIPE,
-                           encoding = 'utf-8')
+                           stderr = subprocess.PIPE)
     if reply.returncode == 0:
         return True, reply.stdout
     else:
         return False, reply.stdout + reply.stderr
 
-curDate = subprocess.run('date',
-                      shell = True,
-                      stdout = subprocess.PIPE,
-                      stderr = subprocess.PIPE,
-                      encoding = 'utf-8')
-
-with open('check_ip_list', r) as chkList, \
-     open('{date}_avlb_ip'.format(date=curDate.stdout), w) as avIP, \
-     open('{date}_unlb_ip'.format(date=curDate.stdout), w) as unIP:
-     for ip in chkList:
-         answer = ping_ip(ip)
-         if answer[0]: avIP.write(ip)
-         else: unIP.write(ip)
+with open('check_ip_list', 'r') as chkList, \
+     open('{}_avlb_ip'.format(str(date.today())), 'w') as avIP, \
+     open('{}_unlb_ip'.format(str(date.today())), 'w') as unIP:
+    for ip in chkList:
+        answer = ping_ip(ip)
+        if answer[0]: avIP.write(ip)
+        else: unIP.write(ip)
             
